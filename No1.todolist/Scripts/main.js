ce4ac7f09b3d2242ec7af3df5e75445842fd8c1c
Po-CHEN LEE <asd06a@yahoo.com.tw>
2018-06-10 21:02:26 +0800

    header change

--- No1.todolist/Scripts/main.js
+++ No1.todolist/Scripts/main.js
@@ -1,3 +1,11 @@
 ﻿$(document).ready(function () {
+    $(".tabs").click(function (e) {
+        e.preventDefault();
+        let tabIdx = $(this).index();
 
-});
+        $(this).siblings().removeClass("tabs__activate");
+        $(this).addClass("tabs__activate");
+        $(".main").fadeOut(400)
+            .eq(tabIdx).delay(400).fadeIn();
+    });
+});;
