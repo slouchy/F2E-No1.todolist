c4f9b9ba6a806c040386be63cc79a34dffebc55a
Po-CHEN LEE <asd06a@yahoo.com.tw>
2018-06-10 23:12:24 +0800

    complete page finish

--- No1.todolist/Scripts/main.js
+++ No1.todolist/Scripts/main.js
@@ -8,4 +8,26 @@
         $(".main").fadeOut(400)
             .eq(tabIdx).delay(400).fadeIn();
     });
+
+    $(".btn-highlight").click(function (e) {
+        e.preventDefault();
+
+        $(this)
+            .find("span")
+            .toggleClass("glyphicon-star")
+            .toggleClass("glyphicon-star-empty")
+            .toggleClass("task-list-button__highlight")
+            .parents(".main-content-task-ul-item")
+            .toggleClass("main-content-task-ul-item__highlight")
+    });
+
+    $("input[type='checkbox']").change(function (e) {
+        e.preventDefault();
+
+        if ($(this).is(":checked")) {
+            $(this).parents(".main-content-task-ul-item").find(".task-list-txt").addClass("task-list-txt__completed");
+        } else {
+            $(this).parents(".main-content-task-ul-item").find(".task-list-txt").removeClass("task-list-txt__completed");
+        }
+    });
 });;
